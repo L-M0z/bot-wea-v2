3ceedcc8a2c19aa11758eda3b2b6b1fde882fd02
L-M0z <79562661+L-M0z@users.noreply.github.com>
2021-02-24 13:10:58 +0700

    help.js

--- Fxc7/help.js
+++ Fxc7/help.js
@@ -26,104 +26,30 @@ prefix " ${prefix} "
 ❐ *tanggal*  : *${tanggal}*
 ❐  *runtime* : *${kyun(uptime)}
 
-╔[ *¢MENU BOT* ]
-║
-╠➤ ${prefix}playmp3 ( link )
-╠➤ ${prefix}anjing
-╠➤ ${prefix}kucing
-╠➤ ${prefix}toimg ( reply sticker
-╠➤ ${prefix}tupai ( reply vn/audio)
-╠➤ ${prefix}bass ( reply vn/audio
-╠➤ ${prefix}tahta
-╠➤ ${prefix}ttp
-╠➤ ${prefix}tts
-╠➤ ${prefix}persengay
-╠➤ ${prefix}pbucin
-╠➤ ${prefix}katailham
-╠➤ ${prefix}pantum
-╠➤ ${prefix}katacinta ( alay anjing )
-╠➤ ${prefix}bucin
-╠➤ ${prefix}katabijak
-╚➤ ${prefix}fakta
+Welcome to Bot simple by Julian x L-M0
 
-╔[ *¢Fun Group* ]
-║
-╠➤ ${prefix}PornHub step brother
-╠➤ ${prefix}gifstiker
-╠➤ ${prefix}sticker
-╠➤ ${prefix}Dorking
-╠➤ ${prefix}hilih
-╠➤ ${prefix}fitnah
-╠➤ ${prefix}tebakgambar
-╠➤ ${prefix}darkjokes
-╠➤ ${prefix}xvideos japan
-╠➤ ${prefix}playstore nama aplikasi
-╠➤ ${prefix}igstalk apa ignya
-╠➤ ${prefix}brainly pertanyaan
-╠➤ ${prefix}mimpi kepala charger
-╠➤ ${prefix}artinama siapa nama lu
-╠➤ ${prefix}jamdunia Jakarta misalnya
-╠➤ ${prefix}jadwaltv mnctv
-╚➤ ${prefix}caklontong
+Berikut menu simple bot Tirs
 
-╔[ *GrupMenu* ]
-║
-╠➤ ${prefix}tagme
-╠➤ ${prefix}kick @member
-╠➤ ${prefix}demote @admin
-╠➤ ${prefix}promote @member
-╠➤ ${prefix}tagall
-╠➤ ${prefix}hidetag
-╠➤ ${prefix}ownergrup
-╠➤ ${prefix}welcome on╠➤ ${prefix}off
-╚➤ ${prefix}grup buka╠➤ ${prefix}tutup ( kalo bot jadi admin
-
-¢Hanya owner yang bisa akses menu ini
-
-➤ ${prefix}ownermenu (private)
-
-╔[ *18+ menu om :v* ]
-║
-╠➤ ${prefix}nsfw on/off
-╠➤ ${prefix}modeanime on/(on kan dulu)
-╠➤ ${prefix}hentai
-╠➤ ${prefix}nsfwblowjob
-╠➤ ${prefix}nsfwloli
-╚➤ ${prefix}nsfwneko
-
-╔[ *WeeaBoo menu anjing* ]
-║
-╠➤ ${prefix}boruto
-╠➤ ${prefix}naruto
-╠➤ ${prefix}minato
-╠➤ ${prefix}hinata
-╠➤ ${prefix}sakura
-╠➤ ${prefix}sasuke
-╠➤ ${prefix}toukachan
-╠➤ ${prefix}rize
-╠➤ ${prefix}akira
-╠➤ ${prefix}itori
-╠➤ ${prefix}kurumi
-╠➤ ${prefix}miku
-╠➤ ${prefix}anime
-╠➤ ${prefix}animecry
-╠➤ ${prefix}animekiss
-╚➤ ${prefix}neonime
-
-╔[ *Menu Ngontol* ]
-║
-╠➤ ${prefix}spamcall 08xxxxx
-╚➤ ${prefix}spamgmail bapaklu@gmail.com
-
-╔[ *creator* ]
-║
-╠➤ *O BOT*
-╠➤ *M. HADI FIRMANSYA*
-╠➤ *DELIA AULIA*
-╠➤ *KEVIN DAVID*
-╠➤ *FXC7*
-╠➤ *L-M0* 
-╚➤ *PANJUL* 
+┏[ *MENU* ]
+┃
+┃${prefix} ×#sticker
+┃${prefix} ×#ownermenu ( khusus owner )
+┃${prefix} ×#ytmp3 (Link)
+┃${prefix} ×#attp ( kata katanya )
+┃${prefix} ×#stickergif
+┃${prefix} ×#fitnah @tagmember|katakata|balasanbot
+┃${prefix} ×#toimg
+┃${prefix} ×#owner
+┃${prefix} ×#infobot
+┃${prefix} ×#donasi
+┃${prefix} ×#runtime
+┃${prefix} ×#igstalk
+┣━━» *THANGKS TO* 
+┃
+┣➤ *LM0* 
+┣➤ *JULIAN* 
+┃
+┗━━[ *Simple-Bot* ]   
 `
 }
 
